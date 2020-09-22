5fc42568ca6d537811df64d6a1156556fbfa051f
xujiaming02 <>
2020-09-22 11:00:40 +0800

    a

--- myCC.go
+++ myCC.go
@@ -1,9 +1,10 @@
 master
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
+Copyright IBM Corp. 2016 All Rights Reserved.
+sdfsd
+sdfsdf
+sdf
 You may obtain a copy of the License at
 
 		 http://www.apache.org/licenses/LICENSE-2.0
